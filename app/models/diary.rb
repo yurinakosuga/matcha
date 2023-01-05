f295c603a4a58fe2ea5e30eb8e675203949b6627
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2023-01-05 05:38:32 +0000

    diaries new.html

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,3 +1,9 @@
 class Diary < ApplicationRecord
   enum week: { credit_card: 0, transfer: 1 }
+  enum time: { weekday: 0, weekend_holiday: 1 }
+  enum how_to_use: { for_here: 0, to_go: 1 }
+  enum waiting_time: { none: 0, 15minutes: 1, 30minutes: 2, 45minutes: 3, 1hour: 4 }
+  enum amount: { small: 0, medium: 1, large: 2 }
+  enum sweetness: { sweet: 0, just_right: 1, not_sweet: 2 }
+ 
 end
