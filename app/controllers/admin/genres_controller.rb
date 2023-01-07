8b072d6b52ec0956d3afafb3fd18b54a3b48ad8f
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2023-01-07 12:29:05 +0000

    admin/genres,shops

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,42 @@
 class Admin::GenresController < ApplicationController
+  
   def index
-  end
-
-  def edit
+    @genre = Genre.new
+    @genres = Genre.all
+    
   end
   
   def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    genre = Genre.new(genre_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    genre.save
+    
+    redirect_to '/admin/genres'
+    
+  end
+  
+  def edit
+    @genre = Genre.find(params[:id])
+    
+  end
+  
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to '/admin/genres'   
+  end
+  
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to '/admin/genres'
+  end
+  
+   private
+  # ストロングパラメータ
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 
-  def update
-  end
 end
--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -1,13 +1,42 @@
 class Admin::ShopsController < ApplicationController
+  
   def index
-  end
-
-  def edit
+    @shop = Shop.new
+    @shops = Shops.all
+    
   end
   
   def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    shop = Shop.new(shop_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    shop.save
+    
+    redirect_to '/admin/shops'
+    
+  end
+  
+  def edit
+    @shop = Shop.find(params[:id])
+    
+  end
+  
+  def update
+    shop = Shop.find(params[:id])
+    shop.update(shop_params)
+    redirect_to '/admin/shops'   
+  end
+  
+  def destroy
+    @shop = Shop.find(params[:id])
+    @shop.destroy
+    redirect_to '/admin/shops'
+  end
+  
+   private
+  # ストロングパラメータ
+  def shop_params
+    params.require(:shop).permit(:name)
   end
 
-  def update
-  end
 end
