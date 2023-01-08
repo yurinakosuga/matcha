29426a6c158161943994fb6afb73675a65360c24
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2023-01-08 12:51:03 +0000

    diaries/show

--- app/controllers/public/diaries_controller.rb
+++ app/controllers/public/diaries_controller.rb
@@ -19,11 +19,12 @@ class Public::DiariesController < ApplicationController
   
   def create
     @diary = Diary.new(diary_params)
-    #@diary.user_id = current_user.id
+    @diary.user_id = current_user.id
+    @diary.date = "#{params[:date_year]}-#{params[:date_month]}-#{params[:date_day]} 00:00:00"
     @diary.save
     
     if @diary.save
-      redirect_to '/'
+      redirect_to diary_path(@diary.id)
     else
       render new
     end
@@ -36,7 +37,7 @@ class Public::DiariesController < ApplicationController
   end
   
   def diary_params
-    params.require(:diary).permit(:user_id, :shop_id, :number_of_times, :date, :week, :time, :number_of_people, :how_to_use, :waiting_time, :genre_id, :price, :amount, :darkness, :sweetness, :comprehensive_evaluation)
+    params.require(:diary).permit(:user_id, :shop_id, :number_of_times, :week, :time, :number_of_people, :how_to_use, :waiting_time, :genre_id, :price, :amount, :darkness, :sweetness, :comprehensive_evaluation, :image)
   end
 
 end
--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,7 @@
 class Diary < ApplicationRecord
+  has_one_attached :image
+  belongs_to  :shop
+  
   enum week: { weekday: 0, weekend_holiday: 1 }
   enum time: { morning: 0, pm_12: 1, pm_14: 2, pm_16: 3, pm_18: 4 }
   enum how_to_use: { for_here: 0, to_go: 1 }
@@ -6,4 +9,13 @@ class Diary < ApplicationRecord
   enum amount: { small: 0, medium: 1, large: 2 }
   enum sweetness: { sweet: 0, just_right: 1, not_sweet: 2 }
   enum price: { yen_500: 0, yen_1000: 1, yen_1500: 2, yen_2000: 3 }
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [100, 100]).processed
+  end
+  
 end
