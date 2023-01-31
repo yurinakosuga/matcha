06fb901a4f20ce1f7dfc96fa8e74336ca10ba93b
EC2 Default User <ec2-user@ip-172-31-10-61.ap-northeast-1.compute.internal>
2023-01-31 13:13:35 +0000

    テストデータ挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-   email: 'test@test.com',
-   password: 'test1234',
-   
-)
+   Admin.create!(
+      email: 'test@test.com',
+      password: 'test1234',
+      
+   )
+
+
+num = 1
+while num < 10 do 
+   User.create(id: num, email: "#{num}@gmail.com", nickname: "hoge", address: 0, last_name_kana: "hoge", first_name_kana: "hoge", age: 1, gender: 0, password: "hoghoeghoe")
+   num = num + 1
+end
+
+
+
+
+["hoge", "fuga", "higa"].each.with_index(1) do |name, index| 
+   Shop.create(id: index, name: name )
+end
+
+
+
+num = 1
+while num < 10 do 
+   Diary.create(id: num, user_id: num, shop_id:)
+   num = num + 1
+end
+
+
+
+
+
+
