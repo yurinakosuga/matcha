d662e1cbe4f96db2e6f595a673e5fd5c2fabf33c
EC2 Default User <ec2-user@ip-172-31-10-61.ap-northeast-1.compute.internal>
2023-02-01 04:19:36 +0000

    テストデータ挿入(diaries)

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,27 @@
 
 num = 1
 while num < 10 do 
-   User.create(id: num, email: "#{num}@gmail.com", nickname: "hoge", address: 0, last_name_kana: "hoge", first_name_kana: "hoge", age: 1, gender: 0, password: "hoghoeghoe")
+   User.create(id: num, 
+               email: "#{num}@gmail.com", 
+               last_name_kana: "hoge", 
+               first_name_kana: "hoge",
+               nickname: "hoge",
+               gender: 0,
+               age: 1, 
+               address: 0, 
+               password: "hoghoeghoe") 
    num = num + 1
+end            
+                
+               
+
+
+["パフェ", "ケーキ", "プリン", "アイスクリーム", "その他"].each.with_index(1) do |name, index| 
+   Genre.create(id: index, name: name )
 end
 
 
-
-
-["hoge", "fuga", "higa"].each.with_index(1) do |name, index| 
+["中村藤吉", "祇園辻利", "寿月堂", "ナナズグリーンティ", "その他"].each.with_index(1) do |name, index| 
    Shop.create(id: index, name: name )
 end
 
@@ -29,7 +42,22 @@ end
 
 num = 1
 while num < 10 do 
-   Diary.create(id: num, user_id: num, shop_id:)
+   Diary.create(id: num,
+                user_id: num, 
+                shop_id: 1, 
+                number_of_times: "1",
+                week: 1, 
+                time: 1, 
+                number_of_people: 1, 
+                how_to_use: 1,
+                waiting_time: 1,
+                genre_id: 1,
+                price: 1,
+                amount: 1,
+                darkness: 5,
+                sweetness: 3,
+                comprehensive_evaluation: 5,
+                free_writing: "抹茶が濃くておいしかったです")
    num = num + 1
 end
 
